f234215669c10624e301d7802b8733f5339421a1
Alex Krysov <krysov-95@mail.ru>
2022-12-13 15:49:11 +0600

    added tests

---
+++ src/test/java/com/krysov/websteps/WebTest.java
@@ -0,0 +1,21 @@
+package com.krysov.websteps;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+public class WebTest {
+    private static final String REPOSITORY = "AlexKrysov/hw9_allure";
+    private static final int ISSUE = 1;
+    @Test
+    public void testAnnotatedStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithNumber(ISSUE);
+    }
+}
